f7a7708b0d5aac3ca2f15e0003b95b250d6f3b27
Rupa Dhillon <rupa211984@gmail.com>
2019-06-07 02:23:28 -0700

    Show exceptions

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,22 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    app.call(env)
+  rescue Exception => err
+    render_exception(err)
   end
 
   private
 
   def render_exception(e)
+    ["500", {'Content-type' => 'text/html'}, [e.message, " <br>", e.backtrace.join("<br>")]]
   end
 
 end
