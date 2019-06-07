849783926960e0dae93966ea829b6bfbe90f7172
Rupa Dhillon <rupa211984@gmail.com>
2019-06-07 02:57:38 -0700

    Serve static files from public

--- lib/static.rb
+++ lib/static.rb
@@ -1,7 +1,20 @@
 class Static
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    req = Rack::Request.new(env)
+    fpath = /.+public\/(.+)/.match(req.path)[1]
+    ext = /.+\.(\w+)/.match(fpath)
+    file = File.read("public/#{fpath}")
+
+    ["200", {'Content-type' => "image/#{ext}"}, [file]]
+  rescue Exception => err
+    [404, {'Content-type' => "text/html"}, [err.message]]
+  ensure
+    app.call(env)
   end
 end
